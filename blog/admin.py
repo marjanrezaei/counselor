25027f9cd9ac6d0db747a9c55da9f3c1bc8808ae
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-23 22:11:42 +0330

    manage  post admin
    manage post in admin by add class AdminPost in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from blog.models import Post,Category
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display =('title', 'author', 'status', 'published_date', 'content_view')
+    empty_value_dispaly = '-empty-'
+    # fields = ('title',)
+    # exclude = ('title',)
+    list_filter = ('status', 'author')
+    # ordering = ('-published_date',)
+    search_fields = ('title', 'content')
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
