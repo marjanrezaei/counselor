39aec1c431dcf28f592b618efcf68d352999c6fa
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-23 22:19:40 +0330

    add contact table
    1. add contact class to models in website
    2. add  ContactAdmin class to admin.py and register contact

--- blog/admin.py
+++ blog/admin.py
@@ -6,10 +6,7 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     list_display =('title', 'author', 'status', 'published_date', 'content_view')
     empty_value_dispaly = '-empty-'
-    # fields = ('title',)
-    # exclude = ('title',)
     list_filter = ('status', 'author')
-    # ordering = ('-published_date',)
     search_fields = ('title', 'content')
 
 admin.site.register(Post, PostAdmin)
