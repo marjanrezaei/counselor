e930a0ce12905bb09c4de51fd1bd3255bfbf69bc
marjanrezaei <rezaei.marjann@gmail.com>
2024-12-24 20:44:27 +0330

    set static for image in all page

--- counselor/settings.py
+++ counselor/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,9 +123,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'statics',
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'statics')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'website'
 
 urlpatterns = [
-    path('',views.index, name = 'index'),
+    path('', views.index, name = 'index'),
     path('about/', views.about, name = 'about'),
     path('contact/', views.contact, name = 'contact'),
     path('counselor/', views.counselor, name = 'counselor'),
